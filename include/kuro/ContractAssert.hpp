c12d9ec96a671686edbcff770c3fb3c300d44e56
Patrick Schwartz <kurogit@schwartzm.com>
2016-08-20 19:28:32 +0200

    Add contract_assertion_mode enum
    * Test the existence of all required enum members.

--- include/kuro/ContractAssert.hpp
+++ include/kuro/ContractAssert.hpp
@@ -2,4 +2,16 @@
 
 namespace kuro
 {
+
+/*!
+ * \brief Enum containing the possible build modes for contract assertions
+ * \details Passed as part of contract_violation_info to the current contract violation handler
+ */
+enum class contract_assertion_mode
+{
+    opt,
+    test,
+    safe
+};
+
 }  // namespace kuro
--- test/src/KuroAssertTest.cpp
+++ test/src/KuroAssertTest.cpp
@@ -1,6 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("test")
+#include <kuro/ContractAssert.hpp>
+
+TEST_CASE("test_contract_assertion_mode")
 {
+    using kuro::contract_assertion_mode;
+
+    contract_assertion_mode m = contract_assertion_mode::opt;
+    (void)m;
+    m = contract_assertion_mode::test;
+    (void)m;
+    m = contract_assertion_mode::safe;
+    (void)m;
 }
