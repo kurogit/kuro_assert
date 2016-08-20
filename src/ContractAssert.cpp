07f1609d9b24c198281b8ceb8aff3937cb8e00f8
Patrick Schwartz <kurogit@schwartzm.com>
2016-08-20 20:17:15 +0200

    Add set_ and get_contract_violation_header

--- src/ContractAssert.cpp
+++ src/ContractAssert.cpp
@@ -2,4 +2,24 @@
 
 namespace kuro
 {
+
+namespace
+{
+contract_violation_handler globalHandler = nullptr;
+}  // namespace
+
+contract_violation_handler get_contract_violation_handler() noexcept
+{
+    return globalHandler;
+}
+
+contract_violation_handler set_contract_violation_handler(contract_violation_handler handler) noexcept
+{
+    auto prevHandler = globalHandler;
+
+    globalHandler = handler;
+
+    return prevHandler;
+}
+
 }  // namespace kuro
--- test/src/KuroAssertTest.cpp
+++ test/src/KuroAssertTest.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+#include <type_traits>
+
 #include <kuro/ContractAssert.hpp>
 
 TEST_CASE("test_contract_assertion_mode")
@@ -33,5 +35,33 @@ TEST_CASE("test_contract_violation_handler_matches_specification")
     using kuro::contract_violation_handler;
     using kuro::contract_violation_info;
 
-    CHECK((std::is_same<kuro::contract_violation_handler, void (*) (const contract_violation_info&)>::value));
+    CHECK((std::is_same<kuro::contract_violation_handler, void (*)(const contract_violation_info&)>::value));
+}
+
+void test_handler(kuro::contract_violation_info const&)
+{
+}
+void test_handler_2(kuro::contract_violation_info const&)
+{
+}
+
+TEST_CASE("test_set_and_get_contract_violation_handler")
+{
+    using kuro::get_contract_violation_handler;
+    using kuro::set_contract_violation_handler;
+
+    // calling get_contract_violation_handler first, returns nullptr
+    CHECK_FALSE(get_contract_violation_handler());
+
+    // Setting the first handler return nullptr
+    CHECK_FALSE(set_contract_violation_handler(&test_handler));
+
+    // After setting a handler it should be returned
+    CHECK(get_contract_violation_handler() == &test_handler);
+
+    // If another handler is set the previous one should be retuerned
+    CHECK(set_contract_violation_handler(&test_handler_2) == &test_handler);
+
+    // And returned again
+    CHECK(get_contract_violation_handler() == &test_handler_2);
 }
